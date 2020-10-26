91290e2c9649e183a3ccbc5b2486026015bd76f1
Henrik Witt-Hansen <hwha@dbc.dk>
2020-10-26 12:14:41 +0100

    Add missing source headers
    Auditors: jbn, atm, jbr

--- src/main/java/dk/dbc/opensearch/workpresentation/model/WorkPresentationSubject.java
+++ src/main/java/dk/dbc/opensearch/workpresentation/model/WorkPresentationSubject.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
 package dk.dbc.opensearch.workpresentation.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
